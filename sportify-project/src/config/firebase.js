696e3a87b2e481cfbafdb1700e847c58f44bcf22
swells2020 <wells.callum@outlook.com>
2022-08-09 10:13:33 +0100

    adds firebase setup

---
+++ sportify-project/src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl_jx511Y2vu7JWhg9Ypt4QQ-L6a8TGUk",
+  authDomain: "sportify-project-dev.firebaseapp.com",
+  projectId: "sportify-project-dev",
+  storageBucket: "sportify-project-dev.appspot.com",
+  messagingSenderId: "340906617637",
+  appId: "1:340906617637:web:8f9c77e980879f4f4308cc",
+  measurementId: "G-7XNVFVDC8G"
+};
+
+const app = initializeApp(firebaseConfig);
