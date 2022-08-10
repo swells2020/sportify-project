143b6531f7c3ee3ae5ce91b7d13aa807fb29f4bb
Ben <benjamurph@hotmail.co.uk>
2022-08-10 09:52:29 +0100

    Converted login and create account to modals

--- sportify-project/src/react-apps/Unauthorised.js
+++ sportify-project/src/react-apps/Unauthorised.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link, Routes, Route } from "react-router-dom";
-import { Button, Container } from "react-bootstrap";
-
+import { Button, Container, Modal } from "react-bootstrap";
+import { useState } from "react"
 import Home from "../react-pages/Home";
 import SignUp from "../react-components/Signup";
 import Login from "../react-components/LogIn";
 import PasswordReset from "../react-components/PasswordReset";
 
 const Unauthorised = (props) => {
+  const [show, setShow] = useState(false);
+  const [loginComponent, setLoginComponent] = useState('Log In')
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div>
       <Container>
@@ -17,12 +23,14 @@ const Unauthorised = (props) => {
           <Link to="/home">
             <Button>Home</Button>
           </Link>
-          <Link to="/signup">
-            <Button>SignUp</Button>
-          </Link>
-          <Link to="/login">
-            <Button>LogIn</Button>
-          </Link>
+          <Button variant="primary" onClick={handleShow}>
+          Login
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+        {(loginComponent === 'Log In') ? <Login setLoginComponent={setLoginComponent} /> : null}
+        {(loginComponent === 'Sign Up') ? <SignUp setLoginComponent={setLoginComponent} /> : null}
+        </Modal>
           <Link to="/passwordreset">
             <Button>PasswordReset</Button>
           </Link>
