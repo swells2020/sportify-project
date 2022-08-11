14652c3e8f3169a8d4ce1063084d254145ab72f5
Paul-Ramshaw <ramshaw@hotmail.co.uk>
2022-08-11 16:26:11 +0100

    add scheduled events sorted by date

--- sportify-project/src/react-components/Schedule.jsx
+++ sportify-project/src/react-components/Schedule.jsx
@@ -1,23 +1,37 @@
-import { useEffect, useState } from "react";
-import { getDoc, doc } from "firebase/firestore";
-import { db } from "../config/firebase";
-import { useContext } from "react";
-import UserContext from "../react-contexts/UserContext";
-import ScheduleList from "../react-components/ScheduleList";
+import { useEffect, useState } from 'react';
+import { getDoc, doc } from 'firebase/firestore';
+import { db } from '../config/firebase';
+import { useContext } from 'react';
+import UserContext from '../react-contexts/UserContext';
+import ScheduleList from '../react-components/ScheduleList';
 
 function Schedule() {
   const user = useContext(UserContext);
   const [schedule, setSchedule] = useState([]);
   useEffect(() => {
     if (user.events) {
-      user.events.forEach((event) => {
-        const docRef = doc(db, "events", event);
-        getDoc(docRef).then((data) => {
-          setSchedule((prev) => {
-            return [...prev, { ...data.data(), eventId: event }];
-          });
-        });
+      const eventRequests = user.events.map((event) => {
+        const docRef = doc(db, 'events', event);
+        return getDoc(docRef);
       });
+
+      Promise.all(eventRequests)
+        .then((data) => {
+          const scheduledEvents = data.map((scheduledEvent) => {
+            return scheduledEvent.data();
+          });
+
+          scheduledEvents.sort(function (a, b) {
+            return (
+              new Date(a.date.seconds * 1000) - new Date(b.date.seconds * 1000)
+            );
+          });
+
+          setSchedule([...scheduledEvents]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }, [user]);
   return (
